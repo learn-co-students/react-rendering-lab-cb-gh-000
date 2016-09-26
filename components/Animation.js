872d7b26da7845c19fe06a2f4f2d04651ea5a958
Kadi Kraman <kadi.kraman@red-badger.com>
2016-09-26 19:17:12 +0100

    Create a place for componentWillUpdate()

---
+++ components/Animation.js
@@ -0,0 +1,16 @@
+const React = require('react');
+
+class Animation extends React.Component {
+
+  // TODO: create a componentWillUpdate() here
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+module.exports = Animation;
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const Translator = require('./components/Translator');
 const Circles = require('./components/Circles');
 const Pusheen = require('./components/Pusheen');
+const Animation = require('./components/Animation');
 const React = require('react');
 const ReactDOM = require('react-dom');
 
 ReactDOM.render(
   <div>
-    <div className="container"><Translator /></div>
-    <div className="container"><Circles /></div>
-    <div className="container"><Pusheen /></div>
+    <div className="container">
+      <div className="title">componentWillReceiveProps()</div>
+      <Translator />
+    </div>
+    <div className="container">
+      <div className="title">shouldComponentUpdate()</div>
+      <Circles />
+    </div>
+    <div className="container">
+      <div className="title">componentWillUpdate()</div>
+      <Animation />
+    </div>
+    <div className="container">
+      <div className="title">componentDidUpdate()</div>
+      <Pusheen />
+    </div>
   </div>,
   document.getElementById('main')
 );
