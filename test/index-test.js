07e4d6cd30b7586a82edfce60066d0cfa1683b34
Luke Ghenco <lukeghenco@gmail.com>
2017-06-06 13:41:09 -0400

    Update tests to use chai

--- test/index-test.js
+++ test/index-test.js
@@ -1,55 +1,39 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 
-import Rating from '../components/Rating';
-import Circle from '../components/Circle';
-import Animation from '../components/Animation';
-import Pikachu from '../components/Pikachu';
+import Rating from '../src/components/Rating';
+import Circle from '../src/components/Circle';
+import Animation from '../src/components/Animation';
+import Pikachu from '../src/components/Pikachu';
 
 
 describe('Rating', () => {
   it('sets the increasing state correctly', () => {
     const wrapper = shallow(<Rating rating={1} />);
     wrapper.setProps({ rating: 2 });
-    expect(wrapper.state()).toEqual({ increasing: true, decreasing: false });
+    expect(wrapper.state()).to.deep.equal({ increasing: true, decreasing: false });
   });
 
   it('sets the increasing state correctly', () => {
     const wrapper = shallow(<Rating rating={1} />);
     wrapper.setProps({ rating: -4 });
-    expect(wrapper.state()).toEqual({ increasing: false, decreasing: true });
+    expect(wrapper.state()).to.deep.equal({ increasing: false, decreasing: true });
   });
 
   it('sets the stable state correctly', () => {
     const wrapper = shallow(<Rating rating={1} />);
     wrapper.setProps({ rating: 1 });
-    expect(wrapper.state()).toEqual({ increasing: false, decreasing: false });
+    expect(wrapper.state()).to.deep.equal({ increasing: false, decreasing: false });
   });
 });
 
 describe('Circle', () => {
   it('does not re-render when the same color is passed in twice', () => {
-    const spy = expect.spyOn(Circle.prototype, 'render').andCallThrough();
+    const spy = sinon.spy(Circle.prototype, 'render')
     const wrapper = shallow(<Circle color='red' />);
     wrapper.setProps({ color: 'red' });
-    expect(spy.calls.length).toEqual(1);
-  });
-});
-
-describe('Animation', () => {
-  it('triggers the loading bar when a rerender is about to take place ', () => {
-    const spy = expect.spyOn(Animation.prototype, 'showLoadingBar');
-    const wrapper = shallow(<Animation />);
-    wrapper.instance().componentWillUpdate();
-    expect(spy.calls.length).toEqual(1);
-  });
-});
-
-describe('Pikachu', () => {
-  it('calls the function to resize the pikachu whenever the component re-renders', () => {
-    const spy = expect.spyOn(Pikachu.prototype, 'resizePikachu');
-    const wrapper = shallow(<Pikachu />);
-    wrapper.instance().componentDidUpdate();
-    expect(spy.calls.length).toEqual(1);
+    expect(spy.calledOnce).to.be.true;
   });
 });
