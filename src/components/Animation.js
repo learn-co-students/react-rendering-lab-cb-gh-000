d1437a9e48d2be3c7393b94af4e1a0748f249e3d
github username <andrewcohn0@gmail.com>
2018-01-12 10:26:56 -0500

    keeping these gifs g rated

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,42 +1,48 @@
-import React from 'react';
+import React from "react";
 
 class Animation extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      url: ' http://placehold.it/500x150'
+      url: " http://placehold.it/500x150",
     };
   }
 
   getNewCat = () => {
-    fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+    fetch("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC", {
+      fmt: "json",
+      rating: "g",
+    })
       .then((res, err) => {
         if (err) {
-          console.log('Something went wrong with fetching your new cat!', err)
+          console.log("Something went wrong with fetching your new cat!", err);
         } else {
-          return res.json()
+          return res.json();
         }
       })
-      .then(result => this.setState({
-        url: result.data.fixed_height_downsampled_url
-      }));
-  }
+      .then(result =>
+        this.setState({
+          url: result.data.fixed_height_downsampled_url,
+        })
+      );
+  };
 
   showLoadingBar = () => {
-    const progressBar = document.getElementById('progress-bar');
-    progressBar.className = 'off on';
-    setTimeout(() => progressBar.className = 'off', 1100);
-  }
+    const progressBar = document.getElementById("progress-bar");
+    progressBar.className = "off on";
+    setTimeout(() => (progressBar.className = "off"), 1100);
+  };
 
   render() {
     return (
       <div>
-        <img src={this.state.url} height="100px"/>
-        <div><button onClick={this.getNewCat}>New random .gif!</button></div>
+        <img src={this.state.url} height="100px" />
+        <div>
+          <button onClick={this.getNewCat}>New random .gif!</button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
