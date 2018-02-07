c56e9e46813808723e158e04689b879b2e8fe751
Matt Lawford <mlawford7@gmail.com>
2018-02-07 10:38:50 -0500

    new tests with all tests passing

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -9,6 +9,10 @@ class Animation extends React.Component {
     };
   }
 
+  componentWillUpdate(){
+    this.showLoadingBar()
+  }
+
   getNewCat = () => {
     fetch("http://api.giphy.com/v1/gifs/random?rating=g&api_key=dc6zaTOxFJmzC")
       .then((res, err) => {
--- src/components/Circle.js
+++ src/components/Circle.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 class Circle extends React.Component {
 
+  shouldComponentUpdate(nextProps) {
+   return Boolean(nextProps.color !== this.props.color);
+ }
+
   render() {
     const { color } = this.props;
     console.log(`A ${color} circle was rendered!`);
-
     return (
       <div className={`circle ${color}`}></div>
     )
