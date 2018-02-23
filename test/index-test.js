a385154608f9bb4ded8b6d85e1322c6958f6e0d3
Matt Lawford <Matt@nyc-055668-mbp.local>
2018-02-23 09:51:07 -0500

    master fixed to match solution

--- test/index-test.js
+++ test/index-test.js
@@ -47,7 +47,7 @@ describe('Animation', () => {
 
     const wrapper = shallow(<Animation/>);
     const spy = sinon.spy(wrapper.instance(), 'showLoadingBar')
-    wrapper.instance().componentWillUpate()
+    wrapper.instance().componentWillUpdate()
     expect(spy.calledOnce).to.be.true
     selector.restore()
   });
